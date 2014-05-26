96b18537cfd617625095d484ce7144a301524af4
Tobias Arud <me@tobiasarud.se>
2014-05-26 21:50:32 +0200

    Some stuff
    More to come later.

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+$to = "tobias@eruma.se";
+$subject = ($_POST['senderName']);
+$message = ($_POST['senderMsg']);
+$message .= "\n\n---------------------------\n";
+$message .= "E-mail Sent From: " . $_POST['senderName'] . " <" . $_POST['senderEmail'] . ">\n";
+$headers = "From: " . $_POST['senderName'] . " <" . $_POST['senderEmail'] . ">\n";
+if(@mail($to, $subject, $message, $headers))
+{
+echo "answer=ok";
+}
+else
+{
+echo "answer=error";
+}
+?>
